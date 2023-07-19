9e78a12445b7dc39ad4bfbf0c300dfe54ed132f4
pekkykang <51907597+pekkykang@users.noreply.github.com>
2023-07-19 19:14:29 +0800

    Add files via upload

---
+++ cycleGAN_dependence.py
@@ -0,0 +1,266 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+import glob
+import random
+import os
+
+from torch.utils.data import Dataset
+from PIL import Image
+import torchvision.transforms as transforms
+
+import random
+import time
+import datetime
+import sys
+
+from torch.autograd import Variable
+import torch
+import numpy as np
+
+from torchvision.utils import save_image
+
+
+
+
+def weights_init_normal(m):
+    classname = m.__class__.__name__
+    if classname.find("Conv") != -1:
+        torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
+        if hasattr(m, "bias") and m.bias is not None:
+            torch.nn.init.constant_(m.bias.data, 0.0)
+    elif classname.find("BatchNorm2d") != -1:
+        torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
+        torch.nn.init.constant_(m.bias.data, 0.0)
+
+
+##############################
+#           RESNET
+##############################
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_features):
+        super(ResidualBlock, self).__init__()
+
+        self.block = nn.Sequential(
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(in_features, in_features, 3),
+            nn.InstanceNorm2d(in_features),
+            nn.ReLU(inplace=True),
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(in_features, in_features, 3),
+            nn.InstanceNorm2d(in_features),
+        )
+
+    def forward(self, x):
+        return x + self.block(x)
+
+
+
+
+class GeneratorResNet(nn.Module):
+    def __init__(self, input_shape, num_residual_blocks):
+        super(GeneratorResNet, self).__init__()
+      
+        channels = input_shape[0]
+        out_features = 64
+        # Initial convolution block
+        self.l_initial = nn.Sequential(
+            nn.ReflectionPad2d(4),
+            nn.Conv2d(channels, out_features, kernel_size= (7,7)),
+            nn.InstanceNorm2d(out_features),
+            nn.ReLU(inplace=True),
+        )
+
+
+        in_features = out_features
+        # Downsampling
+        out_features *= 2
+        self.ds_1 = nn.Sequential(
+            nn.Conv2d(in_features, out_features, 3, stride=2, padding=1),
+            nn.InstanceNorm2d(out_features),
+            nn.ReLU(inplace=True),
+        )
+        in_features = out_features
+        out_features *= 2
+        self.ds_2 = nn.Sequential(
+            nn.Conv2d(in_features, out_features, 3, stride=2, padding=1),
+            nn.InstanceNorm2d(out_features),
+            nn.ReLU(inplace=True),
+        )
+        in_features = out_features
+        # Residual blocks
+        self.re_1 = ResidualBlock(out_features)
+        self.re_2 = ResidualBlock(out_features)
+        self.re_3 = ResidualBlock(out_features)
+        self.re_4 = ResidualBlock(out_features)
+
+        # Upsampling
+        out_features //= 2
+        self.us_1 = nn.Sequential(
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(in_features, out_features, 3, stride=1, padding=1),
+            nn.InstanceNorm2d(out_features),
+            nn.ReLU(inplace=True),
+        )
+        in_features = out_features
+
+        out_features //= 2
+        self.us_2 = nn.Sequential(
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(in_features, out_features, 3, stride=1, padding=1),
+            nn.InstanceNorm2d(out_features),
+            nn.ReLU(inplace=True),
+        )
+        in_features = out_features
+
+        # Output layer
+
+        self.out = nn.Sequential(
+            nn.ReflectionPad2d(channels),
+            nn.Conv2d(out_features, channels, 7),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        x = torch.unsqueeze(x, 1)
+        #print(x.shape)
+        x = self.l_initial(x)
+        #print(x.shape)
+        x = self.ds_1(x)
+        #print(x.shape)
+        x = self.ds_2(x)
+        #print(x.shape)
+        x = self.re_1(x)
+        #print(x.shape)
+        x = self.re_2(x)
+        #print(x.shape)
+        x = self.re_3(x)
+        #print(x.shape)
+        x = self.re_4(x)
+        #print(x.shape)
+        x = self.us_1(x)
+        #print(x.shape)
+        x = self.us_2(x)
+        #print(x.shape)
+        x = self.out(x)
+        #print(x.shape)
+        x = x.squeeze(0)
+        return x
+
+
+##############################
+#        Discriminator
+##############################
+
+
+class Discriminator(nn.Module):
+    def __init__(self, input_shape):
+        super(Discriminator, self).__init__()
+
+        channels, height, width = input_shape
+
+        # Calculate output shape of image discriminator (PatchGAN)
+        self.output_shape = (1, height // 2 ** 4, width // 2 ** 4)
+
+        def discriminator_block(in_filters, out_filters, normalize=True):
+            """Returns downsampling layers of each discriminator block"""
+            layers = [nn.Conv2d(in_filters, out_filters, 4, stride=2, padding=1)]
+            if normalize:
+                layers.append(nn.InstanceNorm2d(out_filters))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            return layers
+
+        self.model = nn.Sequential(
+            *discriminator_block(channels, 64, normalize=False),
+            *discriminator_block(64, 128),
+            *discriminator_block(128, 256),
+            *discriminator_block(256, 512),
+            nn.ZeroPad2d((1, 0, 1, 0)),
+            nn.Conv2d(512, 1, 4, padding=1)
+        )
+
+    def forward(self, img):
+        img = torch.unsqueeze(img, 1)
+        return self.model(img)
+
+
+
+
+def to_rgb(image):
+    rgb_image = Image.new("RGB", image.size)
+    rgb_image.paste(image)
+    return rgb_image
+
+
+class ImageDataset(Dataset):
+
+    def __init__(self, root, transforms_=None, unaligned=False, mode="train"):
+        self.transform = transforms.Compose(transforms_)
+        self.unaligned = unaligned
+
+        self.files_A = sorted(glob.glob(os.path.join(root, "%s/A" % mode) + "/*.*"))
+        self.files_B = sorted(glob.glob(os.path.join(root, "%s/B" % mode) + "/*.*"))
+
+    def __getitem__(self, index):
+        image_A = Image.open(self.files_A[index % len(self.files_A)])
+
+        if self.unaligned:
+            image_B = Image.open(self.files_B[random.randint(0, len(self.files_B) - 1)])
+        else:
+            image_B = Image.open(self.files_B[index % len(self.files_B)])
+
+        # Convert grayscale images to rgb
+        if image_A.mode != "RGB":
+            image_A = to_rgb(image_A)
+        if image_B.mode != "RGB":
+            image_B = to_rgb(image_B)
+
+        item_A = self.transform(image_A)
+        item_B = self.transform(image_B)
+        return {"A": item_A, "B": item_B}
+
+    def __len__(self):
+        return max(len(self.files_A), len(self.files_B))
+
+
+
+
+class ReplayBuffer:
+    def __init__(self, max_size=50):
+        assert max_size > 0, "Empty buffer or trying to create a black hole. Be careful."
+        self.max_size = max_size
+        self.data = []
+
+    def push_and_pop(self, data):
+        to_return = []
+        for element in data.data:
+            element = torch.unsqueeze(element, 0)
+            if len(self.data) < self.max_size:
+                self.data.append(element)
+                to_return.append(element)
+            else:
+                if random.uniform(0, 1) > 0.5:
+                    i = random.randint(0, self.max_size - 1)
+                    to_return.append(self.data[i].clone())
+                    self.data[i] = element
+                else:
+                    to_return.append(element)
+        return Variable(torch.cat(to_return))
+
+
+class LambdaLR:
+    def __init__(self, n_epochs, offset, decay_start_epoch):
+        assert (n_epochs - decay_start_epoch) > 0, "Decay must start before the training session ends!"
+        self.n_epochs = n_epochs
+        self.offset = offset
+        self.decay_start_epoch = decay_start_epoch
+
+    def step(self, epoch):
+        return 1.0 - max(0, epoch + self.offset - self.decay_start_epoch) / (self.n_epochs - self.decay_start_epoch)
+
+
+
+
+
